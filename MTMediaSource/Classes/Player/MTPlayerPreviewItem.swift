a06b3bfb6ff96b452986d13a2c7516c9c552059d
PanGu <pg@PanGudeMac-mini.local>
2022-09-19 11:16:25 +0800

    preview model

---
+++ MTMediaSource/Classes/Player/MTPlayerPreviewItem.swift
@@ -0,0 +1,25 @@
+//
+//  MTPlayerPreviewItem.swift
+//  MTMediaSource
+//
+//  Created by PanGu on 2022/9/19.
+/*
+    预览项
+ */
+
+import Foundation
+
+
+public class MTPlayerPreviewItem{
+    public var filename: String?
+    public var fileId: String!
+    public var fileUrl: String!
+    public var headerStr: String?
+    public var isEncryted: Bool = false
+    public var loadingTips: [String] = []
+    public var bufferModel: MTFkBufferModel?
+    public var source: String?
+    public var fileetag: String?
+    public var fileidx: String?
+    public var isLocalPath: Bool = false
+}
